f326bfc514b0f8a1fa11709456d3910f0659616d
sajeek0944 <sajeel6y4z@gmail.com>
2024-12-24 22:23:11 +0500

    class 14

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,8 @@
 
 import Link from "next/link"
 
-import {client} from "@/sanity/lib/client"
 
 export default async function Home() {
-  // use sanity
-  const query = `*[ _type == "pet"]`;
-  const pet = await client.fetch(query);
-
 
   const apiUrl = await fetch("https://jsonplaceholder.typicode.com/users");
   const get = await apiUrl.json();
@@ -58,69 +53,6 @@ export default async function Home() {
         </div>
       </div>
 
-      {/* <h1>post</h1>
-      <h1>comments</h1>
-      <h1>albums</h1>
-      <h1>photos</h1>
-      <h1>todos</h1>
-      <h1>users</h1> */}
-      {/* {get.map((aa:any)=>(
-        <>
-        <div>
-          <span>{aa.id}</span>  .
-          <span> name {aa.name}</span>
-        </div>
-
-        <div>
-          <span>{aa.id}</span>  .
-          <span> username: {aa.username}</span>
-        </div>
-        <div>
-          <span>{aa.id}</span>  .
-          <span> email: {aa.email}</span>
-        </div>
-        <div>
-          <span>{aa.id}</span>  .
-          <span>address.street:  {aa.address.street}</span>
-        </div>
-        <div>
-          <span>{aa.id}</span>  .
-          <span> address.suite: {aa.address.suite}</span>
-        </div>
-        <div>
-          <span>{aa.id}</span>  .
-          <span> address.city:  {aa.address.city}</span>
-        </div>
-        <div>
-          <span>{aa.id}</span>  .
-          <span> address.zipcode: {aa.address.zipcode}</span>
-        </div>
-        <div>
-          <span>{aa.id}</span>  .
-          <span> address.geo.lat: {aa.address.geo.lat} .... address.geo.lng:  {aa.address.geo.lng}</span>
-        </div>
-        <div>
-          <span>{aa.id}</span>  .
-          <span> phone: {aa.phone}</span>
-        </div>
-        <div>
-          <span>{aa.id}</span>  .
-          <span> website: {aa.website}</span>
-        </div>
-        <div>
-          <span>{aa.id}</span>  .
-          <span> company.name: {aa.company.name}</span>
-        </div>
-        <div>
-          <span>{aa.id}</span>  .
-          <span> company.catchPhrase:  {aa.company.catchPhrase}</span>
-        </div>
-        <div>
-          <span>{aa.id}</span>  .
-          <span> company.bs:  {aa.company.bs}</span>
-        </div>
-        </>
-      ))} */}
     </>
   );
 }
--- src/sanity/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import pet from "../pet"
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [pet],
-}
